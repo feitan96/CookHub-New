32f2f94e88099ca03e7256a4000c83896d1594a8
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-04-19 19:24:43 +0800

    added logo for signup and top bar

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -78,6 +78,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
+      <img src="/assets/images/cookhub.png" alt="logo" />
         
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
